2d500169923c8d0313f795b1c6c76858d0cc3988
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-08 08:04:46 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -20,8 +20,10 @@ print("repo cloud")
 async def verify_webhook(
     mode: str = Query(..., alias="hub.mode"),
     challenge: str = Query(..., alias="hub.challenge"),
-    verify_token: str = Query(..., alias="hub.verify_token")
+    verify_token: str = Query(..., alias="hub.verify_token"),
+    mensaje: str = Query(..., alias="mensaje")
 ):
+    print("mensaje:",mensaje)
     print("get")
     print("modo:",mode)
     print(challenge)
