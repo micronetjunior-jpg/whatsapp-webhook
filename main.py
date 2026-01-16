504c28ec162d57507c6d797fd2981373c061bfee
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-16 01:18:22 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -156,8 +156,11 @@ def procesarPregunta(mensaje: str, telefono: str):
     
     print("Se procede a remitir respuesta a",telefono)
     enviar_mensaje(telefono,respuestaIA)
-    
-    if obtener_estado("tipo_respuesta") == "audio":
+
+    tipo = obtener_estado("tipo_respuesta")
+    print(tipo)
+
+    if tipo == "audio":
         responder_con_audio(telefono,respuestaIA)
         guardar_estado("tipo_respuesta", "IDLE")
         
