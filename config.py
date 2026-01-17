7eedb739d0aefc188a483531e5ca49495fb8eed7
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-17 12:20:08 -0500

    Crear config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+KOKOROURL = os.getenv("KOKOROURL")
+
+REDIS_URL = os.getenv("REDIS_URL")
+
+PRESENTON_INTERNAL_URL = "http://presentonpresentonlatest:3000"
+PRESENTON_PUBLIC_URL = "https://presentonpresentonlatest-ambienteprueba.up.railway.app"
