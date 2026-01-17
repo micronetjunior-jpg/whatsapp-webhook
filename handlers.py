8015b04c15e9eb2ef2e43563fbe39dfb8232840a
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-17 13:10:16 -0500

    Actualizar handlers.py

--- handlers.py
+++ handlers.py
@@ -6,6 +6,9 @@ from services.tts import *
 
 async def handle_message(data):
     msg = data["entry"][0]["changes"][0]["value"]["messages"][0]
+    
+    print("MSG:,msg)
+    
     telefono = msg["from"]
 
     if not acquire_user_lock(telefono):
