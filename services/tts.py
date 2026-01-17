c6fc45d3d832fe81d03949a5af92b7d5b4a58204
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-17 16:51:31 -0500

    Actualizar tts.py

--- services/tts.py
+++ services/tts.py
@@ -2,10 +2,31 @@ import requests
 from config import KOKOROURL
 
 def text_to_speech(texto):
-    r = requests.post(
+    payload = {
+        "model": "kokoro",
+        "input": texto,
+        "voice": "af_heart",
+        "response_format": "mp3",
+        "download_format": "mp3",
+        "speed": 1,
+        "stream": False,  # 👈 importante
+        "return_download_link": False,
+        "lang_code": "es",
+        "volume_multiplier": 1,
+        "normalization_options": {
+            "normalize": True,
+            "unit_normalization": False,
+            "url_normalization": True,
+            "email_normalization": True,
+            "optional_pluralization_normalization": True,
+            "phone_normalization": True,
+            "replace_remaining_symbols": True
+        }
+    }
+    response = requests.post(
         f"{KOKOROURL}/v1/audio/speech",
-        json={"model": "kokoro", "input": texto},
+        json=payload,
         timeout=60
     )
-    r.raise_for_status()
-    return r.content
+    response.raise_for_status()
+    return response.content
