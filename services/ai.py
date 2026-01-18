68b631c9820dabd129cbcfb53325a9549dbaee4a
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-18 07:17:39 -0500

    Actualizar ai.py

--- services/ai.py
+++ services/ai.py
@@ -4,6 +4,13 @@ from memory import obtener_historial, guardar_historial
 
 openai.api_key = OPENAI_API_KEY
 
+modeloTexto = "gpt-5-nano"
+modeloImagen = "gpt-image-1-mini"
+modeloTran = "gpt-4o-mini-transcribe"
+modeloSpeech = "gpt-4o-mini-tts-2025-12-15"
+modeloRealtime = "gpt-realtime-mini-2025-12-15"
+modeloAudio = "gpt-4o-mini-audio-preview-2024-12-17"
+
 def ask_ai(telefono, texto):
     historial = obtener_historial(telefono)
     if not historial:
@@ -12,7 +19,7 @@ def ask_ai(telefono, texto):
     historial.append({"role": "user", "content": texto})
 
     res = openai.chat.completions.create(
-        model="gpt-4o-mini",
+        model=modeloTexto,
         messages=historial,
         max_tokens=1000
     )
@@ -33,7 +40,28 @@ def transcribir_audio(audio_bytes: bytes) -> str:
 
         transcription = client.audio.transcriptions.create(
             file=open(f.name, "rb"),
-            model="gpt-4o-mini-transcribe"
+            model=modeloTran 
         )
+
+    return transcription.text
+    
+client = OpenAI(api_key=OPENAI_API_KEY)
+def texro_a_audio(texto: str) -> str:
+    historial = obtener_historial(telefono)
+    if not historial:
+        historial = [{"role": "system", "content": "Eres un asistente útil. Genera las respuestas sin pies de página, los encabezados, asteriscos o signos"}]
+
+    historial.append({"role": "user", "content": texto})
+
+    res = openai.chat.completions.create(
+        model=modeloTexto,
+        messages=historial,
+        max_tokens=1000
+    )
+
+    respuesta = res.choices[0].message.content
+    historial.append({"role": "assistant", "content": respuesta})
+    guardar_historial(telefono, historial)
+    return respuesta
 
     return transcription.text
