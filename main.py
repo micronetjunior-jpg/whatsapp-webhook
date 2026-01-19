5030f7f8626d0604505ce62e493f6dec05eac3fa
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-18 22:51:37 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request, Response
 from config import VERIFY_TOKEN
 from handlers import handle_message
 from fastapi.responses import JSONResponse
+from audio_ws import *
 
 app = FastAPI()
 
@@ -12,12 +13,6 @@ async def verify(request: Request):
     if p.get("hub.verify_token") == VERIFY_TOKEN:
         return Response(content=p.get("hub.challenge"))
     return Response(status_code=403)
-
-@app.post("/webhook")
-async def webhook(request: Request):
-    data = await request.json()
-    asyncio.create_task(handle_message(data))
-    return Response(status_code=200)
 
 @app.post("/webhook")
 async def meta_webhook(request: Request):
@@ -34,6 +29,12 @@ async def meta_webhook(request: Request):
                 "url": "wss://TU-SERVICIO.railway.app/audio"
             }
         })
-    print("llamada")
-    return JSONResponse({"status": "ok"})
+        print("llamada")
+        return JSONResponse({"status": "ok"})
+    else:
+        data = await request.json()
+        asyncio.create_task(handle_message(data))
+        return Response(status_code=200)
+
+    
 
