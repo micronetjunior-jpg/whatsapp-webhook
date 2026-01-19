64d395167616f50c42e34e6b670a09cb9dfaef48
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-19 13:25:08 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -7,43 +7,22 @@ from config import *
 
 app = FastAPI()
 
-MODEL = "gpt-realtime-mini"
-REALTIME_URL = "wss://api.openai.com/v1/realtime?model="+MODEL 
-
-"""
-async def call_realtime(prompt: str) -> str:
-    response_text = ""
+async def audio_endpoint(ws: WebSocket):
+    await ws.accept()
+    print("🔗 WebSocket /audio conectado")
     async with websockets.connect(
-        REALTIME_URL,
-        extra_headers=HEADERS)
-    as ws:
-        await ws.send(json.dumps({
-            "type": "session.create",
-            "session": {
-            "modalities": ["text"],
-            "instructions": "Eres un asistente experto en agentes inteligentes."} }))
-        await ws.send(json.dumps({
-            "type": "input_text_buffer.append",
-            "text": prompt }))
-        await ws.send(json.dumps({
-            "type": "input_text_buffer.commit"}))
-        await ws.send(json.dumps({
-            "type": "response.create" }))
-
-    while True:
-        event = json.loads(await ws.recv())
-        if event["type"] == "response.output_text.delta":
-            response_text += event["delta"]
-        if event["type"] == "response.completed":
-            break
-        return response_text
+        "wss://api.openai.com/v1/realtime?model=gpt-realtime-mini",
+        extra_headers={
+            "Authorization": f"Bearer {OPENAI_API_KEY}",
+            "OpenAI-Beta": "realtime=v1"}) 
+        
+    
 """
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
-    #user_text = data.get("text", "Hola")
-    #reply = await call_realtime(user_text)
-    print(data)
-    print()
-    #return {"reply": reply}
+    try:
+        #user_text = data.get("text", "Hola")
+        reply = await call_realtime(user_text)
+    return {"reply": reply}
 
