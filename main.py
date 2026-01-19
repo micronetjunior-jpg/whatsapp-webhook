4f88f4a94f956ca018627f0a00e5eaa493f57b36
micronetjunior-jpg <micronetjunior@icloud.com>
2026-01-19 00:44:23 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ async def verify(request: Request):
     if p.get("hub.verify_token") == VERIFY_TOKEN:
         return Response(content=p.get("hub.challenge"))
     return Response(status_code=403)
-
     else:
         data = await request.json()
         asyncio.create_task(handle_message(data))
         return Response(status_code=200)
 
-    
 
+
+async def test():
+    async with websockets.connect(
+        "wss://webhook-server-ambientepruebayy.up.railway.app/audio"
+    ) as ws:
+        print("Conectado al WS")
+        await asyncio.sleep(10)
+
+asyncio.run(test())
